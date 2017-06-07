23b9459be3bdd1f5758f9c4b3eed49c85dba4e32
Thibault Saunier <thibault.saunier@osg.samsung.com>
2017-06-07 16:31:16 -0400

    Rename GStreamer plugin to match so filename
    Making it compatible with GStreamer 1.12

--- gst/transcode/gsttranscodebin.c
+++ gst/transcode/gsttranscodebin.c
@@ -534,6 +534,6 @@ plugin_init (GstPlugin * plugin)
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    transcoding,
+    transcode,
     "A plugin containing elements for transcoding", plugin_init, VERSION,
     GST_LICENSE, GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN)
